95f1cc469cb2d91b05dfce5ebf86b2236190fda9
Harsh <gupt.rahul1234@gmail.com>
2026-07-18 18:14:18 +0530

    added session3

---
+++ react/session3_practice/src/App.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import UseState_arr from './components/UseState_arr'
+import FormHandling from './components/FormHandling'
+import UseEffect_ex from './components/UseEffect_ex'
+import './App.css'
+import 'bootstrap/dist/css/bootstrap.min.css'
+
+const App = () => {
+  return (
+    <>
+      <UseState_arr/>
+      <FormHandling/>
+      <UseEffect_ex/>
+    </>
+  )
+}
+
+export default App
